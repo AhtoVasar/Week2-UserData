4ebc23acc895cad1389753b07c6c9d32c462908c
Ahto Vasar <avasar@tktk.ee>
2021-09-17 23:43:59 +0300

    control flow

---
+++ DrivingLicens/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DrivingLicens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Palun, sisesta oma sünniaasta:");
+            int yearOfBirth = Convert.ToInt32(Console.ReadLine());
+
+            int userAge = 2021 - yearOfBirth;
+
+            if (userAge > 18)
+            {
+                Console.WriteLine("Oled piisavalt vana, et juhiluba saada");
+
+            }
+            else if (userAge < 18)
+            {
+                Console.WriteLine("Oled liiga noor, et juhiluba saada");
+            }
+            else
+            {
+                Console.WriteLine("Palju õnne!!! Nüüd saad juhiluba taodelda");
+            }
+            Console.WriteLine("Kena päeva!");
+
+        }
+    }
+}
+
---
+++ UserAge/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UserAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Palun, sisesta oma vanus");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            int yearOfBirth = 2021 - age;
+
+            Console.WriteLine($"Oled sündinud aastal {yearOfBirth} ");
+
+        }
+    }
+}
